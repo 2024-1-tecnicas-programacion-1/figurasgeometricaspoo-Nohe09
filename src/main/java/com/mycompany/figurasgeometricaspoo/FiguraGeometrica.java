53aeced27a072199e49ef784d9791957f24bb58a
a <a@gmail.com>
2024-05-06 08:57:51 -0500

    herencia

--- src/main/java/com/mycompany/figurasgeometricaspoo/FiguraGeometrica.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/FiguraGeometrica.java
@@ -1,5 +1,34 @@
 package com.mycompany.figurasgeometricaspoo;
 
 public class FiguraGeometrica {
+    private String nombre;
+    private String color;
+    
+    public double obetenerArea(){
+        return 0;
+    }
+    public double obetenerPermitero(){
+        return 0;
+    }
+    public FiguraGeometrica(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
     
 }
