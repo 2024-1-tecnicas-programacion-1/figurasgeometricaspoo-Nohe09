c2724a049a5e900bc227797e84e7e665cb7f53a6
a <a@gmail.com>
2024-05-06 08:57:40 -0500

    herencia

--- src/main/java/com/mycompany/figurasgeometricaspoo/Circulo.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/Circulo.java
@@ -1,5 +1,17 @@
 package com.mycompany.figurasgeometricaspoo;
 
-public class Circulo {
+public class Circulo extends FiguraGeometrica {
     
+     private double radio;
+    
+    public Circulo(String nombre, String color , double radio) {
+        super(nombre, color);
+        this.radio=radio;
+    }
+    public double obetenerArea(){
+        return Math.PI *this.radio *radio;
+    }
+    public double obtenerPerimetro(){
+       return Math.PI*this.radio;
+    }
 }
