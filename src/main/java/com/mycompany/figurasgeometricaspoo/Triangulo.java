093e0ea36ae8d25a35975cf94c2d553cffe03d66
a <a@gmail.com>
2024-05-06 08:58:14 -0500

    herencia

--- src/main/java/com/mycompany/figurasgeometricaspoo/Triangulo.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/Triangulo.java
@@ -1,5 +1,19 @@
 package com.mycompany.figurasgeometricaspoo;
 
-public class Triangulo {
+public class Triangulo extends FiguraGeometrica {
+     private double area;
+    private double altura;
+    public Triangulo(String nombre, String color,double area , double altura ) {
+        super(nombre, color);
+        this.area=area;
+        this.altura=altura;
+    }
+    
+    public double obtenerArea(){
+        return this.area*this.altura/2;
+    }
+    public double obtenerPerimetro(){
+        return this.area+this.altura;
+    }
     
 }
